3eaaf87fa7a042a8d128108231fbee8736ec7ae0
Khaled Alarda <khalid.jamal.4002@gmail.com>
2025-10-11 08:01:19 +0300

    scene 5

---
+++ js/scene5.js
@@ -0,0 +1,122 @@
+// Scene5-specific animations and effects
+const scene5 = document.querySelector(".scene5");
+
+// Create a timeline for the entire scene animation
+const scene5Timeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: scene5,
+    scrub: 0.3,
+    pin: true,
+  },
+});
+
+// Animate the layers on scroll
+scene5Timeline
+  .to(
+    ".scene5 .layer-city",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    0
+  )
+  .to(
+    ".scene5 .layer-trash",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-colored-bags",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-barrel",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    "<"
+  )
+  .to(
+    ".scene5 .layer-fire",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-fire2",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-fire3",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-fire4",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-houses-fire",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-fire-smoke",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  )
+  .to(
+    ".scene5 .layer-smoke",
+    {
+      duration: 1,
+      opacity: 1,
+      ease: "power2.out",
+    },
+    ">"
+  );
+
+gsap.to(".scene5 .layer-smoke", {
+  x: "10%",
+  duration: 4,
+  ease: "power2.out",
+  yoyo: true,
+  repeat: -1,
+  delay: 1,
+});
